f1271036a4a3e64234c05dcd1fc7f763d89350ce
beginaid <zuka.info.official@gmail.com>
2021-10-22 11:11:55 +0900

    Initial Commit

---
+++ TrailogIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrailogIOS
+//
+//  Created by 石塚　崚斗 on 2021/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
